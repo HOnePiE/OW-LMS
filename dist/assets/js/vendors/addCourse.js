52c2bb85db11b219c962e8b314e9c7610222636d
Phạm Anh Tuấn <phamanhtuan95vn@gmail.com>
2024-02-26 14:42:34 +0700

    add course-resume, update curriculmn function of add-course page, add functions for header navbar

--- dist/assets/js/vendors/addCourse.js
+++ dist/assets/js/vendors/addCourse.js
@@ -1,48 +1,38 @@
-const addLectureModal = document.getElementById("addLectureModal");
+
 const addSectionModal = document.getElementById("addSectionModal");
-
 const addSectionButton = document.getElementById("addSectionButton");
-
 const lectureContainer1 = document.getElementById("lectureContainer");
 const sectionContainer = document.getElementById("sectionContainer")
 const inputSectionName = document.getElementById("inputSectionName");
 
-let collapseListNumber = 0;
-let courseSectionOrder = 0;
-let courseLectureOrder = 0;
-let sectionIdOrder = 0;
-let lectureIdOrder = 0;
-let sectionModelId = 0;
-
-// console.dir(inputLectureName);
-// console.dir(addLectureModal);
-// console.dir(addSectionModal);
-
-let currentSectionId = "";
-
-console.log(document.getElementById("SectionSample").parentElement.id);
+let term = 0;
 
 addSectionButton.addEventListener("click", function (event) {
     console.log("clicked");
     event.preventDefault();
-    sectionIdOrder++
-    var modalId = 'addLectureModal-' + sectionIdOrder;
-    var sectionName = inputSectionName.value;
+    term++
+
+    const modalId = 'addLectureModal-' + term;
+    const inputLectureNameId = 'inputLectureName-' + term;
+    const secTionId = "section-" + term;
+    const courseId = "course-" + term;
+    const courseListId = "courseList-" + term;
+    const sectionName = inputSectionName.value;
     if (sectionName.trim() !== "") {
-        var sectionItem = document.createElement("div");
+        const sectionItem = document.createElement("div");
         sectionItem.innerHTML = `
-        <div id="section-${sectionIdOrder}" class="bg-light rounded p-2 mb-4">
+        <div id="${secTionId}" class="bg-light rounded p-2 mb-4">
         <h4>${sectionName}</h4>
         <!-- List group -->
-        <div class="list-group list-group-flush border-top-0" id="courseList">
-            <div id="course-${courseLectureOrder}">
+        <div class="list-group list-group-flush border-top-0" id="${courseListId}">
+            <div id="${courseId}">
                 
             </div>
         </div>
         <a href="#"  class="btn btn-outline-primary btn-sm mt-3" data-bs-toggle="modal" data-bs-target="#${modalId}">Add Lecture +</a>
     </div>
 
-    <div class="modal fade" id="${modalId}" tabindex="-1" role="dialog" aria-labelledby="addLectureModalLabel" aria-hidden="true" name="lec1">
+    <form class="modal fade" id="${modalId}" tabindex="-1" role="dialog" aria-labelledby="addLectureModalLabel" aria-hidden="true" name="lec1">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -50,66 +40,187 @@ addSectionButton.addEventListener("click", function (event) {
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <input id="inputLectureName" class="form-control mb-3" type="text" placeholder="Add new lecture " />
-                        <button id="addLectureButton" class="btn btn-primary" type="button">Add New Lecture</button>
-                        <button class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">Close</button>
+                        <input id="${inputLectureNameId}" class="input_model form-control mb-3" type="text" placeholder="Add new lecture " />
+                        <button id="addNewLecture_btn" class="addNewLecture_btn btn btn-primary" type="submit">Add New Lecture</button>
+                        <button class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close" type="Button">Close</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
               `;
         sectionContainer.appendChild(sectionItem)
-        // const currentSectionId2 = document.getElementsByClassName('bg-light')[1].id;
-        // console.log(currentSectionId2);
-        // define addLectureButton and inputLectureName before section is renderedx
-        const addLectureButton = document.getElementById("addLectureButton");
-        const inputLectureName = document.getElementById("inputLectureName");
+        inputSectionName.value = "";
 
-        addLectureButton.addEventListener('click', function() {
-            console.log("clicked add lectured");
-            var lectureName = inputLectureName.value;
-            console.log(lectureName);
-            var lectureId = 0;
+        const addLectureModal = document.getElementById(modalId);
+
+        addLectureModal.addEventListener('submit', function (event) {
+            event.preventDefault();
+            term++;
+            const inputLectureName = event.target.querySelector(".input_model");;
+            const lectureName = inputLectureName.value;
+            const lectureId = "lecture-" + term;
+            const collapselistId = "collapselist-" + term;
             if (lectureName.trim() !== "") {
-                var lectureItem = document.createElement("div");
+                const lectureItem = document.createElement("div");
                 lectureItem.innerHTML = `
-                <div class="list-group-item rounded px-3 text-nowrap mb-1" id="${lectureId}">
+            <div class="list-group-item rounded px-3 text-nowrap mb-1" id="${lectureId}">
                 <div class="d-flex align-items-center justify-content-between">
                     <h5 class="mb-0 text-truncate">
                         <a href="#" class="text-inherit">
                             <i class="fe fe-menu me-1 align-middle"></i>
-                            <span class="align-middle">${lectureName}</span>
+                            <span id="lectureName" class="align-middle">${lectureName}</span>
                         </a>
                     </h5>
                     <div>
-                        <a href="#" class="me-1 text-inherit" data-bs-toggle="tooltip" data-placement="top"
+                        <a id="editbtn" href="#" class="me-1 text-inherit" data-bs-toggle="tooltip" data-placement="top"
                             title="Edit">
                             <i class="fe fe-edit fs-6"></i>
                         </a>
-                        <a href="#" class="me-1 text-inherit" data-bs-toggle="tooltip" data-placement="top"
+                        <a id="deleteBtn" href="#" class="me-1 text-inherit" data-bs-toggle="tooltip" data-placement="top"
                             title="Delete">
                             <i class="fe fe-trash-2 fs-6"></i>
                         </a>
                         <a href="#" class="text-inherit" data-bs-toggle="collapse"
-                            data-bs-target="#collapselistFive" aria-expanded="false"
-                            aria-controls="collapselistFive">
+                            data-bs-target="#${collapselistId}" aria-expanded="false"
+                            aria-controls="${collapselistId}">
                             <span class="chevron-arrow"><i class="fe fe-chevron-down"></i></span>
                         </a>
                     </div>
                 </div>
-                <div id="collapselistFive" class="collapse" aria-labelledby="${lectureId}"
-                    data-bs-parent="#courseListSecond">
+                <div id="${collapselistId}" class="collapse" aria-labelledby="${lectureId}"
+                    data-bs-parent="#${courseListId}">
                     <div class="p-md-4 p-2">
-                        <a href="#" class="btn btn-secondary btn-sm">Add Article +</a>
-                        <a href="#" class="btn btn-secondary btn-sm">Add Description +</a>
+                        <a href="#" class="btn btn-secondary btn-sm"}>Add Article +</a>
+                        <a href="#" class="btn btn-secondary btn-sm"data-bs-toggle="modal" data-bs-target="#newCatgory">Add Description +</a>
                     </div>
                 </div>
             </div>
+
+            <div class="modal fade" id="newCatgory" tabindex="-1" role="dialog" aria-labelledby="newCatgoryLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title mb-0" id="newCatgoryLabel">Create New Category</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form class="needs-validation" novalidate>
+              <div class="mb-3 mb-2">
+                <label class="form-label" for="title">
+                  Title
+                  <span class="text-danger">*</span>
+                </label>
+                <input type="text" class="form-control" placeholder="Write a Category" id="title" required />
+                <small>Field must contain a unique value</small>
+                <div class="invalid-feedback">Please enter category.</div>
+              </div>
+              <div class="mb-3 mb-2">
+                <label class="form-label">Slug</label>
+                <label for="basic-url" class="form-label">Your vanity URL</label>
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon3">https://example.com</span>
+                  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" />
+                </div>
+
+                <small>Field must contain a unique value</small>
+              </div>
+              <div class="mb-3 mb-2">
+                <label class="form-label" for="parent">Parent</label>
+                <select class="form-select" id="parent" required>
+                  <option selected value="">Select</option>
+                  <option value="Course">Course</option>
+                  <option value="Tutorial">Tutorial</option>
+                  <option value="Workshop">Workshop</option>
+                  <option value="Company">Company</option>
+                </select>
+              </div>
+              <div class="mb-3 mb-3">
+                <label class="form-label">Description</label>
+                <div id="editor">
+                  <br />
+                  <h4>One Ring to Rule Them All</h4>
+                  <br />
+                  <p>
+                    Three Rings for the
+                    <i>Elven-kingsunder</i>
+                    the sky,
+                    <br />
+                    Seven for the
+                    <u>Dwarf-lords</u>
+                    in halls of stone, Nine for Mortal Men,
+                    <br />
+                    doomed to die, One for the Dark Lord on his dark throne.
+                    <br />
+                    In the Land of Mordor where the Shadows lie.
+                    <br />
+                    <br />
+                  </p>
+                </div>
+              </div>
+              <div class="mb-2">
+                <label class="form-label">Enabled</label>
+                <div class="form-check form-switch">
+                  <input type="checkbox" class="form-check-input" id="customSwitch1" checked />
+                  <label class="form-check-label" for="customSwitch1"></label>
+                </div>
+              </div>
+              <div>
+                <button type="submit" class="btn btn-primary">Add New Category</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
                 `;
-                document.querySelector('#section-' + sectionIdOrder).appendChild(lectureItem);
+                document.querySelector('#' + courseId).appendChild(lectureItem);
+                inputLectureName.value = "";
             }
+
+            document.querySelectorAll('#editbtn').forEach((editButton) => {
+                editButton.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    const lectureElement = event.target.closest('.list-group-item');
+                    const lectureNameElement = lectureElement.querySelector('#lectureName');
+                    const currentLectureName = lectureNameElement.textContent;
+                    const inputElement = document.createElement('input');
+
+                    inputElement.type = 'text';
+                    inputElement.style = 'border: none; border-bottom: 1px solid #000;outline: none;padding:2px; background-color: #1e293b;color: #fff;font-family:"Inter",sans-serif;'
+                    inputElement.value = currentLectureName;
+
+                    lectureNameElement.innerHTML = '';
+                    lectureNameElement.appendChild(inputElement);
+                    inputElement.focus();
+
+                    inputElement.addEventListener('blur', updateLectureName);
+                    inputElement.addEventListener('keydown', (event) => {
+                        if (event.key === 'Enter') {
+                            updateLectureName(event);
+                        }
+                    });
+
+                    function updateLectureName(event) {
+                        event.preventDefault();
+                        const newLectureName = inputElement.value;
+                        lectureNameElement.textContent = newLectureName;
+                        inputElement.removeEventListener('blur', updateLectureName);
+                        inputElement.removeEventListener('keydown', updateLectureName);
+                    }
+                });
+            });
+
+            document.querySelectorAll('#deleteBtn').forEach((deleteButton) => {
+                deleteButton.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    const lectureElement = event.target.closest('.list-group-item');
+                    lectureElement.remove();
+                });
+            });
+
+
         });
     }
 });
-
 
